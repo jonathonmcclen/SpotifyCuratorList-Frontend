68176223746d6f1e3e11d51358f943a06defa5da
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-11 17:26:23 -0600

    init logic +
    + fixed issue of navigation incrementing past 0 and Playlist.all.length

--- index.js
+++ index.js
@@ -1,87 +1,102 @@
-const endpoint = "http://localhost:9393/playlsts"
-const endpoint = "http://localhost:9393/genres/  /playlists"
+const endpoint = "http://localhost:9393/playlist"
+const filterEndpoint = ["http://localhost:9393/genre/", "/playlists"]
+
+var current_playlist_range = [0,26]
 
 window.onload = function(){
-    let search_button = document.getElementById("")
-    search_button.addEventListener('click',function(){ })
+    let search_button = document.getElementById("search_bar")
+    search_button.addEventListener('click',function(){ 
+        document.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+              searchGenre()
+            }
+        });
+    })
+
+    
+    getPlaylists();
+    
 };
 
-
-
-function populateAllPlaylists(){
-    Playlist.all
-}
-
-
-
-function getRequests() {
+function getPlaylists() {
     fetch(endpoint)
-
     .then (function(response) {
         return response.json()
     })
-
     .then(requests => {
-        requests.data.forEach(request => {
-            let newRequest = new Request(request, request.attributes)
-            document.querySelector("#requests-container").innerHTML += newRequest.renderRequest()
-            addHeartListener()
+        //debugger
+        requests.forEach(request => {
+            let newPlaylist = new Playlist(request)
+            //debugger
         })
+        renderPlaylistSlice(current_playlist_range[0],current_playlist_range[1]);
     })
-
 };
 
+function next25Playlists(arg1,arg2){
+    if (current_playlist_range[1] >= Playlist.all.length){
 
-
-function postFetch(name, description, category) {
-    console.log(name, description, category)
-    fetch(endpoint, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify({
-            name: name,
-            description: description,
-            category_id: category
-        })
-    })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(object) {
-        let newRequest = new Request(object, object)
-        document.querySelector("#requests-container").innerHTML += newRequest.renderRequest()
-        addHeartListener()
-    })
-    .catch(function(error) {
-        console.log(error)
-      })
-}
-
-function patchVote(request) {
-    let submitValue 
-    if (request.value == "true") {
-        submitValue = "false"
+    } else if (current_playlist_range[1] + 25 <= 0){
+        renderPlaylistSlice(current_playlist_range[0] += 25, current_playlist_range[1] = Playlist.all.length)
     } else {
-        submitValue = "true"
+        renderPlaylistSlice(current_playlist_range[0] += 25,current_playlist_range[1] += 25 )
     }
-    fetch(endpoint + `/${request.id}`, {
-        method: "PATCH",
-        headers: {
-            "content-type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify({
-            vote: submitValue
-        })
-    }).then(function(response) {
-        return response.json()
-    })
-    .then(function(object) {
-        document.querySelector("#requests-container").innerHTML = ""
-        getRequests()
-        addHeartListener()
+}
+
+function prev25Playlists(){
+    if (current_playlist_range[0] <= 0){
+
+    } else if (current_playlist_range[0] - 25 <= 0){
+        renderPlaylistSlice(current_playlist_range[0] = 0, current_playlist_range[1] = 26 )
+    } else {
+        renderPlaylistSlice(current_playlist_range[0] -= 25, current_playlist_range[1] -= 25 ) 
+    }
+}
+
+function renderPlaylistSlice(arg1,arg2){
+    clearList()
+    Playlist.all.slice(arg1, arg2).forEach( playlist => {
+        playlist.renderPlaylist();
     })
 }
+
+function clearList(){
+    let list = document.getElementById("playlist_list")
+    list.innerHTML = ""
+}
+
+function searchGenre(){
+    Playlist.all = []
+    let searchContent = document.getElementById("search_bar").value
+
+    let genreEndPoint = filterEndpoint[0] + searchContent + filterEndpoint[1]
+
+    fetch(genreEndPoint)
+    .then (function(response) {
+        return response.json()
+    })
+    .then(requests => {
+        requests.forEach(request => {
+            let newPlaylist = new Playlist(request)
+        })
+        current_playlist_range[0] = 0
+        current_playlist_range[1] = 26
+        
+        renderPlaylistSlice(current_playlist_range[0],current_playlist_range[1]);
+    })
+}
+
+function scrollToTop(){
+    window.scrollTo(0, 0)
+}
+
+function highlighter(string, search){
+    if (string.includes(search)){
+        array = string.split(", ")
+        elementI = array.findIndex(search)
+        array[elementI] = `<em>` + array[elementI] + `</em>`
+        newString = array.join(", ")
+        return newString
+    }
+}
+
